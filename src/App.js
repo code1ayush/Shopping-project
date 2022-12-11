a4b53478e3fa11e44b9bde3183435a87261f5582
ayushkumar <ayushverma8900@gmail.com>
2022-12-11 22:26:04 +0530

    styling correction

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState, useEffect,useContext} from 'react'
+import React,{useState, useEffect} from 'react'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
 import Data from './data';
